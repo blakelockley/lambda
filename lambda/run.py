14409c8a9d46d090aa736b51bf225a1578711998
Blake Lockley <bwlockley@gmail.com>
2019-11-19 22:53:05 +1100

    Update run while manual testing

--- lambda/run.py
+++ lambda/run.py
@@ -1,5 +1,16 @@
 from parser import parse
+from debug import print_debug
 
-text = "id = \\x.x"
+text_I = r"I = \x.xy"
+text_0 = r"0 = \s.(\z.z)"
+text_1 = r"1 = \sz.s(z)"
+text_S = r"S = \w.(\y.(\x.y(wyx)))"
 
-parse(text)
+text = text_S
+
+defns = parse(text)
+
+print(text)
+for defn in defns.values():
+    print(repr(defn))
+    print_debug(defn)
