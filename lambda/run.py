cb1c4d953fbc5b9392f569b7ee01750425de440e
Blake Lockley <bwlockley@gmail.com>
2019-11-20 08:56:33 +1100

    Update run for manual testing

--- lambda/run.py
+++ lambda/run.py
@@ -5,8 +5,11 @@ text_I = r"I = \x.xy"
 text_0 = r"0 = \s.(\z.z)"
 text_1 = r"1 = \sz.s(z)"
 text_S = r"S = \w.(\y.(\x.y(wyx)))"
+text_M = r"M = (\xyz.x(yz))(\sz.(sz))(\sz.(sz))"
+text_A = r"A = (\x.x)y"
+text_Z = r"Z = \xy.xyz"
 
-text = text_S
+text = text_M
 
 defns = parse(text)
 
