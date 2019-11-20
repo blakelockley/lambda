f5eeba5c25d2d5f8b5efa4c541d1c45e74e04c37
Blake Lockley <bwlockley@gmail.com>
2019-11-20 16:17:01 +1100

    Update parser handling expressions

--- src/lparser.py
+++ src/lparser.py
@@ -12,12 +12,26 @@ PAT_APPLICATION = re.compile(r"(.+)([a-z]|\\[a-z]+\..+|\(.+\))")
 # Defintion
 PAT_DEFINITION = re.compile(r"([A-Z0-9])\s*=\s*(.+)")  # S = <expr>
 
+# Expressions
+PAT_NESTED_PARANS = re.compile(r"\((.+)\)")
+
 
 class ParserError(Exception):
     pass
 
 
-def split_expressions(text: str):
+def strip_parans(text: str):
+    """Strip nested parans"""
+
+    m = re.fullmatch(PAT_NESTED_PARANS, text)
+    while m:
+        text = m.group(1)
+        m = re.fullmatch(PAT_NESTED_PARANS, text)
+
+    return text
+
+
+def split_expression(text: str):
     """
     Return a list of expr_text at the top level of 'text'.
     """
@@ -25,39 +39,39 @@ def split_expressions(text: str):
     if len(text) == 0:
         raise ParserError("Unable to split expressions from empty string.")
 
-    # Check if the expression is a function, if so return as is
-    if text[0] == "\\":
-        return [text] 
-
     result = []
     start = 0
     counter = 0
 
     for pos, char in enumerate(text):
 
-        if char == "(":
+        # If we find a top level function we can break the loop,
+        #   the remainder of 'text' will be appended to result
+        if char == "\\" and counter == 0:
+            break
+
+        elif char == "(":
             # Check of we finished parsing free text
             if counter == 0:
                 # Check if valid free text
                 if start != pos:
-                    result.append(text[start: pos])
+                    result.append(text[start:pos])
 
                 # Record opening paran pos
                 start = pos
 
             counter += 1
 
-
         elif char == ")":
             counter -= 1
 
             # Check if we finished parsing parans
             if counter == 0:
-                expr_text = text[start + 1 : pos]
+                expr_text = strip_parans(text[start + 1 : pos])
 
                 if len(expr_text) == 0:
-                    raise ParserError(f"Empty parans in snippet {line} are invalid.") 
-                    
+                    raise ParserError(f"Empty parans in snippet {line} are invalid.")
+
                 result.append(expr_text)
 
                 # Resest counter
@@ -75,17 +89,19 @@ def split_expressions(text: str):
 def parse_expression(text: str):
 
     # Top-level (relative) expressions
-    exprs = split_expressions(text)
+    exprs = split_expression(text)
 
+    # Update text with parans stripped
     if len(exprs) == 1:
         text = exprs[0]
 
     elif len(exprs) >= 2:
-        expr = parse_expression(exprs.pop())
+        # TODO: use deque
+        expr = parse_expression(exprs.pop(0))
 
         while len(exprs) > 0:
-            expr_1 = parse_expression(exprs.pop())
-            expr = Application(expr_1, expr)
+            expr_2 = parse_expression(exprs.pop(0))
+            expr = Application(expr, expr_2)
 
         return expr
 
