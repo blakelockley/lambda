d1ee19f1729ba9ea08630906598a7d584f51c3dd
Blake Lockley <bwlockley@gmail.com>
2019-11-21 14:38:58 +1100

    Update REPL

--- src/repl.py
+++ src/repl.py
@@ -3,27 +3,29 @@ from .lreducer import reduce_expression
 from .debug import print_debug
 
 WELCOME_MESSAGE = """* Welcome to the lambda REPL!
-* Hint: You can use '\\' to represent to lambda character (λ).
-"""
+* Hint: You can use '\\' to represent to lambda character (λ)."""
 
-REPL_PROMPT = "λ "
+REPL_PROMPT = ">>> "
 EXIT_COMMANDS = set(["exit", "quit", "q"])
 
 
 def run_repl(args):
     print(WELCOME_MESSAGE)
 
+    # Loop
     while True:
+
+        # Read
         line = input(REPL_PROMPT)
+        line = line.replace("λ", "\\")  # Replace λ with substitued lambda characted
 
         if line in EXIT_COMMANDS:
             break
 
-        # Replace
-        line = line.replace("λ", "\\")
-
+        # Evaluate
         expr = parse_expression(line)
         reduced = reduce_expression(expr)
 
-        output = str(reduced).replace("\\", "λ")
-        print("--> ", output)
+        # Print
+        output = str(reduced).replace("\\", "λ")  # Revert slashes for printing
+        print(output)
